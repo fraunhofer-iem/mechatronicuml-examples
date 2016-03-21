9181e6dacac8ac381af0377efb980f451fdcd968
Ingo Budde <bingo@mail.upb.de>
2016-03-21 08:59:49 +0000

    [Example] Rename top level folder

---
+++ inProgress/org.muml.example.productionBroker/src-gen/root-cic/operations/operations.h
@@ -0,0 +1,24 @@
+#ifndef OPERATIONS_H_
+#define OPERATIONS_H_
+#ifdef __cplusplus
+  extern "C" {
+#endif
+	
+	
+	
+	
+
+	#include "noSQLDatabaseOpRep.h"
+
+	
+	
+	
+	
+
+	#include "iDManagementOpRep.h"
+
+
+#ifdef __cplusplus
+  }
+#endif
+#endif /*OPERATIONS_H_ */
